0e8e8efb4b350d932119824c8e555fbda7ed4707
AayushKamath <aayushkamath21.ak@gmail.com>
2023-05-09 05:27:03 -0400

    Adding prediction technology

--- appname/urls.py
+++ appname/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import stockdata, home
+from .views import stockdata, home, predict
 from . import views
 
 
 urlpatterns = [
     path('', home),
     path('stockdata/<str:stockdata>/', views.stockdata, name='stockdata'),
+    path('predict', views.predict, name='predict')
 ]
--- appname/views.py
+++ appname/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 import pickle
+import os
 import numpy as np
 from django.http import JsonResponse
 from .models import StockData
 from django.views.decorators.csrf import csrf_exempt
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+from tensorflow import keras
+import base64
 
 model = pickle.load(open('appname/modelrf.pkl','rb'))
+model_LSTM = keras.models.load_model('appname/LSTM_80.h5')
+
+def create_sequences(data, t, d):
+    X, y = [], []
+    for i in range(len(data)-t-1):
+        a = data[i:(i+t), :]
+        X.append(a)
+        y.append(data[i + t, :])
+    return np.array(X), np.array(y)
 
 @csrf_exempt
 def stockdata(request, stockdata):
@@ -14,6 +31,62 @@ def stockdata(request, stockdata):
     response['Access-Control-Allow-Origin'] = '*'
 
     return response
+
+@csrf_exempt
+def predict(request):
+    if request.method == 'POST':
+        # get the input from the user
+        print(request)
+        # try:
+        #     print("hello")
+        #     os.remove('appname/inverse_plot.png')
+        # except:
+        #     pass
+        
+
+        csv_file = request.FILES.get('csv_file')
+        df = pd.read_csv(csv_file)
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        df_scaled = scaler.fit_transform(df)
+
+        t = 10 # timesteps
+        d = 9 # dimensions (including year, month, and day)
+        # X_train, y_train = create_sequences(train, t, d)
+        X_test, y_test = create_sequences(df_scaled, t, d)
+
+        test_pred = model_LSTM.predict(X_test)
+
+        testPredict = scaler.inverse_transform(test_pred)
+        testPredictPlot = np.empty_like(df_scaled)
+        testPredictPlot[:, :] = np.nan
+        testPredictPlot[10:len(df_scaled)-1, :] = testPredict
+
+        plt.plot(scaler.inverse_transform(df_scaled)[:,4],'b')
+        plt.plot(testPredictPlot[:,4],'g')
+        plt.xlabel('No of iterations')
+        plt.ylabel('Close value of stock')
+        plt.legend(['Actual value', 'Predicted value'])
+        # plt.show()
+        plt.savefig('appname/inverse_plot.png')
+        plt.close()
+        with open('appname/inverse_plot.png', 'rb') as f:
+            image_data = f.read()
+
+        encoded_image_data = base64.b64encode(image_data).decode('utf-8')
+
+
+        response_data = {
+            'image': f'data:image/png;base64,{encoded_image_data}'
+        }
+        # print(response_data)
+        response = JsonResponse(response_data)
+    # else:
+    #     stock_data = StockData.objects.filter(stock='A').values()
+    #     response = JsonResponse(list(stock_data), safe=False)
+    
+    response['Access-Control-Allow-Origin'] = '*'
+    return response
+
 
 @csrf_exempt
 def home(request):
